ad27307e7fbe31f98fa36ae6bff43ac89f0f9e33
adegokeolujide <adegokeolujidemic@gmail.com>
2022-10-02 18:02:12 +0100

    if statement

---
+++ ifstatement.php
@@ -0,0 +1,28 @@
+<?php  
+
+$ben = 4;
+$mike = 6;
+$ade = 7;
+
+if($ben == $mike && $ade){
+    echo 'It is correct';
+}
+
+if($ben != $mike && $ade){
+    echo 'They are not the same';
+}
+
+if($ben = $mike && $ade){
+    echo 'They are  the same only';
+}
+
+
+
+
+// if($ben == $mike){
+//     echo 'It is correct';
+// }
+
+
+
+?>
