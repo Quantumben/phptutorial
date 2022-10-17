b3b3c9a7f4b7c9998c39279e729b05c8152671df
QuantumBen <omosabenedict@gmail.com>
2022-10-17 17:58:27 +0100

    Assignment

---
+++ welcome.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+Welcome <?php echo $_REQUEST["name"]; ?><br>
+Your email address is: <?php echo $_REQUEST["email"]; ?>
+
+<!-- $_GET
+$_REQUEST
+$_POST -->
+
+
+//FACEBOOK.COM
+Welcome.php //$_session
+fulldetails.php
+
+</body>
+</html>
